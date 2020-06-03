6aafedab867c409a54423bc8a155b94c8d4a4025
Edward Zhang <edwardz123@gmail.com>
2020-06-02 18:31:55 -0700

    add eval

--- train_deepmind.py
+++ train_deepmind.py
@@ -1,10 +1,9 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, RIGHT_ONLY, COMPLEX_MOVEMENT
-from stable_baselines import DQN, ACER
+from stable_baselines import DQN
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.deepq.policies import MlpPolicy, CnnPolicy, LnCnnPolicy
-# from stable_baselines.common.policies import MlpPolicy, CnnPolicy
 from stable_baselines.common.atari_wrappers import FrameStack
 from stable_baselines.common.callbacks import CallbackList, EvalCallback
 from callbacks import ProgressBarManager
@@ -17,18 +16,18 @@ env = gym_super_mario_bros.make('SuperMarioBros-v3')
 env = JoypadSpace(env, RIGHT_ONLY)
 env = FrameStack(env, n_frames=4)
 
-# eval_callback = EvalCallback(env,
-#                              best_model_save_path='./logs/',
-#                              log_path='./logs/',
-#                              eval_freq=1000,
-#                              deterministic=True,
-#                              render=False)
+eval_callback = EvalCallback(env,
+                             best_model_save_path='./logs/',
+                             log_path='./logs/',
+                             eval_freq=1000,
+                             deterministic=True,
+                             render=False)
 
 print("Compiling model...")
 model = DQN(LnCnnPolicy,
             env,
             verbose=1,
-            learning_starts=50000,
+            learning_starts=1000,
             learning_rate=1e-4,
             exploration_final_eps=0.01,
             prioritized_replay=True,
@@ -37,34 +36,11 @@ model = DQN(LnCnnPolicy,
             tensorboard_log="./mario_tensorboard/"
         )
 
-
-'''
-ACER
-  policy: 'CnnPolicy'
-  n_envs: 16
-  n_timesteps: !!float 1e7
-  lr_schedule: 'constant'
-  buffer_size: 5000
-  ent_coef: 0.01
-
-PPO2 
-  n_envs: 8
-  n_steps: 128
-  noptepochs: 4
-  nminibatches: 4
-  n_timesteps: !!float 1e7
-  learning_rate: lin_2.5e-4
-  cliprange: lin_0.1
-  vf_coef: 0.5
-  ent_coef: 0.01
-  cliprange_vf: -1
-'''
-
 print("Training starting...")
-steps = 100000
+steps = 1500
 with ProgressBarManager(steps) as progress_callback:
     model.learn(total_timesteps=steps,
-                callback=[progress_callback],
+                callback=[progress_callback, eval_callback],
                 tb_log_name="altered_reward")
 
 print("Done! Saving model...")
